5d3ba2604a5a7cf92245877487b948cbb07473dd
TopAcademy <dan--di@mail.ru>
2024-05-13 21:59:43 +0300

    13.05.24

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -2,12 +2,18 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'todo/task_detail.html'
+    context_object_name = 'task'
 
 class TaskList(ListView):
     model = Task
@@ -19,5 +25,5 @@ class TaskList(ListView):
         context['desc'] = "It's only test, no more"
         return context
 
-    def get_queryset(self):
-        return Task.objects.filter(completed=False)
+    # def get_queryset(self):
+    #     return Task.objects.filter(completed=False)
