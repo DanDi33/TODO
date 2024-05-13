c38296b9ecfb453c3daadd81465ba69eee646972
TopAcademy <dan--di@mail.ru>
2024-05-13 21:34:56 +0300

    13.05.24

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Task
@@ -10,3 +12,12 @@ def home(request):
 class TaskList(ListView):
     model = Task
     template_name = 'todo/tasks.html'
+    context_object_name = 'tasks'
+
+    def get_context_data(self,* ,object_list = None,**kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['desc'] = "It's only test, no more"
+        return context
+
+    def get_queryset(self):
+        return Task.objects.filter(completed=False)
