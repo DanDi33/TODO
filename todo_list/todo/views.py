60ecfeaee89103f3e5440902c7eb2bca38f76d4d
TopAcademy <dan--di@mail.ru>
2024-05-14 21:28:03 +0300

    14.05.24

--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -1,8 +1,13 @@
 from typing import Any
 from django.db.models.query import QuerySet
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Task
 
 # Create your views here.
@@ -27,3 +32,33 @@ class TaskList(ListView):
 
     # def get_queryset(self):
     #     return Task.objects.filter(completed=False)
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ['title', 'description','completed']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(self.request, 'The task was created succesfully!')
+        return super(TaskCreate, self).form_valid(form)
+    
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description','completed']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The task was updated succesfully!')
+        return super(TaskUpdate, self).form_valid(form)
+    
+class TaskDelete(DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    # context_object_name = 'task'
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The task was deleted succesfully!')
+        return super(TaskDelete, self).form_valid(form)
+
