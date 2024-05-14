83d087777c56c974d10980e9ea7e2bc5563c689d
TopAcademy <dan--di@mail.ru>
2024-05-14 19:06:52 +0300

    14.05.24

--- todo_list/todo/models.py
+++ todo_list/todo/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
-    discription = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
