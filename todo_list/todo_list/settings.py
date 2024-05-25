e7cb9270cf47cf9046f5f22433dc1eee04213a93
DanDi33 <dan--di@mail.ru>
2024-05-25 18:08:38 +0300

    18.05.24

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -127,3 +127,14 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP settings
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = '465'
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+
+EMAIL_HOST_USER = "revi.krim@yandex.ru"
+EMAIL_HOST_PASSWORD = "daserkfshvtokhdk"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
